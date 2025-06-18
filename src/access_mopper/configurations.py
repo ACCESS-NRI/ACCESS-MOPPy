fdf7281e08d7096b1894695c34d5caad7295dade
rhaegar325 <rhaegar227@gmail.com>
2025-06-18 18:51:44 +1000

    update for MOM6 ocean cmorise

--- src/access_mopper/configurations.py
+++ src/access_mopper/configurations.py
@@ -331,7 +331,7 @@ class ACCESS_OM3_CMIP6(CMIP6_Experiment):
         # Not all variable have a time component (e.g. fx, Ofx)
         time_axis = axes.pop("time", None)
         if time_axis:
-            time_axis = axes.pop("time")
+            # time_axis = axes.pop("time")
             time_numeric = ds[time_axis].values
             time_units = ds[time_axis].attrs["units"]
             time_bnds = ds[ds[time_axis].attrs["bounds"]].values
@@ -375,7 +375,7 @@ class ACCESS_OM3_CMIP6(CMIP6_Experiment):
             latitude_vertices=lat_bnds,
             longitude_vertices=lon_bnds,
         )
-        cmor_axes.append(grid_id)
+        # cmor_axes.append(grid_id)
 
         # Now, load the Omon table to set up the time axis and variable
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -387,40 +387,42 @@ class ACCESS_OM3_CMIP6(CMIP6_Experiment):
             cmorTime = cmor.axis(
                 "time", coord_vals=time_numeric, cell_bounds=time_bnds, units=time_units
             )
+            # cmor_axes.append(cmorTime)
+
+        # Append coordinates and transpose data axes 
+        if data.ndim == 3:
+            cmor_axes.append(grid_id)
             cmor_axes.append(cmorTime)
+            data = np.transpose(data, (1, 2, 0))
 
-        if axes:
-            for axis, dim in axes.items():
-                coord_vals = var[dim].values
-                try:
-                    cell_bounds = var[var[dim].attrs["bounds"]].values
-                except KeyError:
-                    cell_bounds = None
-                print(axis, dim)
-                if axis == "depth_coord":
-                    depth = coord_vals
-                    bounds = np.zeros((len(depth), 2))
-                    bounds[1:, 0] = 0.5 * (depth[1:] + depth[:-1])
-                    bounds[:-1, 1] = 0.5 * (depth[1:] + depth[:-1])
-                    bounds[0, 0] = max(0.0, depth[0] - (depth[1] - depth[0]) / 2)
-                    bounds[-1, 1] = depth[-1] + (depth[-1] - depth[-2]) / 2
-                    cell_bounds = bounds
-                # axis_units = var[dim].attrs["units"]
-                # if axis_units == "meter":
-                axis_units = "m"
-                # print("Registering axis:", axis)
-                cmor_axis = cmor.axis(
-                    axis,
-                    coord_vals=coord_vals,
-                    cell_bounds=cell_bounds,
-                    units=axis_units,
-                )
-                cmor_axes.append(cmor_axis)
-
-        cmorTime = cmor.axis(
-            "time", coord_vals=time_numeric, cell_bounds=time_bnds, units=time_units
-        )
-        cmor_axes.append(cmorTime)
+        elif data.ndim == 4:
+            cmor_axes.append(grid_id)
+            if axes:
+                for axis, dim in axes.items():
+                    coord_vals = var[dim].values
+                    try:
+                        cell_bounds = var[var[dim].attrs["bounds"]].values
+                    except KeyError:
+                        cell_bounds = None
+                    print(axis, dim)
+                    if axis == "depth_coord":
+                        depth = coord_vals
+                        bounds = np.zeros((len(depth), 2))
+                        bounds[1:, 0] = 0.5 * (depth[1:] + depth[:-1])
+                        bounds[:-1, 1] = 0.5 * (depth[1:] + depth[:-1])
+                        bounds[0, 0] = max(0.0, depth[0] - (depth[1] - depth[0]) / 2)
+                        bounds[-1, 1] = depth[-1] + (depth[-1] - depth[-2]) / 2
+                        cell_bounds = bounds
+                    axis_units = "m"
+                    cmor_axis = cmor.axis(
+                        axis,
+                        coord_vals=coord_vals,
+                        cell_bounds=cell_bounds,
+                        units=axis_units,
+                    )
+                    cmor_axes.append(cmor_axis)
+            cmor_axes.append(cmorTime)
+            data = np.transpose(data,(2, 3, 1, 0))
 
         # if axes:
         #     for axis, dim in axes.items():
@@ -438,10 +440,7 @@ class ACCESS_OM3_CMIP6(CMIP6_Experiment):
         #             bounds[0, 0] = max(0.0, depth[0] - (depth[1] - depth[0]) / 2)
         #             bounds[-1, 1] = depth[-1] + (depth[-1] - depth[-2]) / 2
         #             cell_bounds = bounds
-        #         # axis_units = var[dim].attrs["units"]
-        #         # if axis_units == "meter":
         #         axis_units = "m"
-        #         # print("Registering axis:", axis)
         #         cmor_axis = cmor.axis(
         #             axis,
         #             coord_vals=coord_vals,
@@ -450,24 +449,17 @@ class ACCESS_OM3_CMIP6(CMIP6_Experiment):
         #         )
         #         cmor_axes.append(cmor_axis)
 
+        # if time_axis:
+        #     cmorTime = cmor.axis(
+        #         "time", coord_vals=time_numeric, cell_bounds=time_bnds, units=time_units
+        #     )
+        #     cmor_axes.append(cmorTime)
+
         # Define CMOR variable
         cmorVar = cmor.variable(cmor_name, variable_units, cmor_axes, positive=positive)
 
         # Write data to CMOR
-<<<<<<< HEAD
-        data = np.moveaxis(data, 0, -1)
-        ntimes_passed = len(time_numeric) if time_axis else 0
-        cmor.write(cmorVar, data, ntimes_passed=ntimes_passed)
-=======
-        print(data.shape)
-        data=np.transpose(data,(2, 3, 1, 0))
-        print(data.shape)
-        # data = np.moveaxis(data, [0, 3], [1, 2])
-        # data = np.moveaxis(data,0, -1)
-        # data = np.moveaxis(data, 0, -1)
-        # cmor.set_cur_dataset_attribute("outpath", "/g/data/tm70/yz9299/ARE")
         cmor.write(cmorVar, data, ntimes_passed=len(time_numeric))
->>>>>>> b256971 (test-version of 3d cmor)
 
         # Finalize and save the file
         filename = cmor.close(cmorVar, file_name=True)
