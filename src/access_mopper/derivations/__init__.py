b2f2ed4154e2b93624c03857458792c603ad374e
rbeucher <romain.beucher@anu.edu.au>
2025-07-24 10:19:48 +1000

    Update mappings

--- src/access_mopper/derivations/__init__.py
+++ src/access_mopper/derivations/__init__.py
@@ -3,6 +3,8 @@ custom_functions = {
     "subtract": lambda a, b: a - b,
     "multiply": lambda a, b: a * b,
     "divide": lambda a, b: a / b,
+    "power": lambda a, b: a**b,
+    "sum": lambda x, **kwargs: x.sum(**kwargs),
     "mean": lambda *args: sum(args) / len(args),
     "kelvin_to_celsius": lambda x: x - 273.15,
     "celsius_to_kelvin": lambda x: x + 273.15,
@@ -14,9 +16,23 @@ def evaluate_expression(expr, context):
         return context[expr]
     elif isinstance(expr, (int, float)):  # literal number
         return expr
+    elif isinstance(expr, list):
+        return [
+            evaluate_expression(item, context)
+            if isinstance(item, (dict, str))
+            else item
+            for item in expr
+        ]
     elif isinstance(expr, dict):
         op = expr["operation"]
-        operands = [evaluate_expression(arg, context) for arg in expr["operands"]]
-        return custom_functions[op](*operands)
+        args = [
+            evaluate_expression(arg, context)
+            for arg in expr.get("args", expr.get("operands", []))
+        ]
+        kwargs = {
+            k: evaluate_expression(v, context)
+            for k, v in expr.get("kwargs", {}).items()
+        }
+        return custom_functions[op](*args, **kwargs)
     else:
         raise ValueError(f"Unsupported expression: {expr}")
