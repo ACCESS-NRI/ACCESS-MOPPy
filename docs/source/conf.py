bb573fc8bb9c311712c3070dd09119e5ae7abcce
rbeucher <romain.beucher@anu.edu.au>
2025-08-04 09:00:53 +1000

    Add Read the Docs configuration and documentation dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,25 +3,107 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(project_root / "src"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "ACCESS-MOPPeR"
-copyright = "2025, Romain Beucher, ACCESS-NRI"
-author = "Romain Beucher"
-release = "2.0.0a"
+copyright = "2025, ACCESS-NRI"
+author = "Romain Beucher, ACCESS-NRI"
+release = "2.0.0-alpha"
+version = "2.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "nbsphinx"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+    "nbsphinx",
+]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "canonical_url": "",
+    "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "style_nav_header_background": "#2980B9",
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
+
+# -- Extension configuration -------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+
+autodoc_typehints = "description"
+autoclass_content = "both"
+autodoc_member_order = "bysource"
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+    "dask": ("https://docs.dask.org/en/stable/", None),
+}
+
+# MyST parser settings
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
+
+# Mock imports for Read the Docs
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if on_rtd:
+    autodoc_mock_imports = [
+        "netCDF4",
+        "cftime",
+        "parsl",
+    ]
+else:
+    autodoc_mock_imports = []
