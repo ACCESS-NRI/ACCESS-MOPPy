f473c990ac5a3559361716ecf4d1df5032156618
rhaegar325 <rhaegar227@gmail.com>
2025-10-10 10:50:08 +1100

    update for grid

--- src/access_moppy/driver.py
+++ src/access_moppy/driver.py
@@ -4,10 +4,9 @@ from typing import Any, Dict, Optional, Union
 
 from access_moppy.atmosphere import CMIP6_Atmosphere_CMORiser
 from access_moppy.defaults import _default_parent_info
-from access_moppy.ocean import CMIP6_Ocean_CMORiser
+from access_moppy.ocean import CMIP6_Ocean_CMORiser_OM2, CMIP6_Ocean_CMORiser_OM3
 from access_moppy.utilities import load_model_mappings
 from access_moppy.vocabulary_processors import CMIP6Vocabulary
-
 
 class ACCESS_ESM_CMORiser:
     """
@@ -86,14 +85,24 @@ class ACCESS_ESM_CMORiser:
                 drs_root=drs_root if drs_root else None,
             )
         elif table in ("Oyr", "Oday", "Omon", "SImon"):
-            self.cmoriser = CMIP6_Ocean_CMORiser(
-                input_paths=self.input_paths,
-                output_path=str(self.output_path),
-                cmor_name=cmor_name,
-                cmip6_vocab=self.vocab,
-                variable_mapping=self.variable_mapping,
-                drs_root=drs_root if drs_root else None,
-            )
+            if self.source_id == "ACCESS-OM3":
+                self.cmoriser = CMIP6_Ocean_CMORiser_OM3(
+                    input_paths=self.input_paths,
+                    output_path=str(self.output_path),
+                    cmor_name=cmor_name,
+                    cmip6_vocab=self.vocab,
+                    variable_mapping=self.variable_mapping,
+                    drs_root=drs_root if drs_root else None,
+                )
+            else:
+                self.cmoriser = CMIP6_Ocean_CMORiser_OM2(
+                    input_paths=self.input_paths,
+                    output_path=str(self.output_path),
+                    cmor_name=cmor_name,
+                    cmip6_vocab=self.vocab,
+                    variable_mapping=self.variable_mapping,
+                    drs_root=drs_root if drs_root else None,
+                )
 
     def __getitem__(self, key):
         return self.cmoriser.ds[key]
