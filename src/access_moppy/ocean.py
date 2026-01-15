5fbd50df1b970904452e588b49506232dcf0d7c2
rhaegar325 <rhaegar227@gmail.com>
2026-01-15 15:47:01 +1100

    mappings and slightly changes in ocean.py for CM3 ocean variables

--- src/access_moppy/ocean.py
+++ src/access_moppy/ocean.py
@@ -226,7 +226,7 @@ class CMIP6_Ocean_CMORiser_OM2(CMIP6_Ocean_CMORiser):
 
     def _get_dim_rename(self):
         """Get the dimension renaming mapping for the grid type."""
-        if "ACCESS-OM2" in self.vocab.source_id:
+        if "ACCESS-OM2" in self.vocab.source_id or "ACCESS-CM" in self.vocab.source_id:
             return {
                 "xt_ocean": "i",
                 "yt_ocean": "j",
@@ -291,7 +291,7 @@ class CMIP6_Ocean_CMORiser_OM3(CMIP6_Ocean_CMORiser):
 
     def _get_dim_rename(self):
         """Get the dimension renaming mapping for the grid type."""
-        if "ACCESS-OM3" in self.vocab.source_id:
+        if "ACCESS-OM3" in self.vocab.source_id or "ACCESS-CM" in self.vocab.source_id:
             return {
                 "xh": "i",
                 "yh": "j",
